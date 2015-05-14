7b4fc3c7e46f3be111010bc68db41f7b6ddf4abc
Huy Phung <huyphung@linux.site>
2015-05-14 11:09:06 +0700

    Remove junk files.

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <sys/time.h>
-
+#include <unistd.h>
 #include "../A5CudaStubs.h"
 
-int main(int argc, char* argv[]) {
-    A5CudaInit(8,12);
+void test0()
+{
     uint64_t plain = 0;
     uint64_t start_val;
     uint64_t stop_val;
@@ -20,29 +20,58 @@ int main(int argc, char* argv[]) {
     }
     samples -= 63;
     int submitted = 0;
-    struct timeval start_time;
-    gettimeofday(&start_time, NULL);
-    for (int i = 0; i < samples; i++) {
-        uint64_t plain = 0;
-        uint64_t plainrev = 0;
-        for (int j = 0; j < 64; j++) {
-            if (plaintext[i + j] == '1') {
-                plain = plain | (1ULL << j);
-                plainrev = plainrev | (1ULL << (63 - j));
+    for (int count = 0; count < 9; count ++) {
+        for (int i = 0; i < samples; i++) {
+            uint64_t plain = 0;
+            uint64_t plainrev = 0;
+            for (int j = 0; j < 64; j++) {
+                if (plaintext[i + j] == '1') {
+                    plain = plain | (1ULL << j);
+                    plainrev = plainrev | (1ULL << (63 - j));
+                }
+            }
+            for (int k = 0; k < 8; k++) {
+                A5CudaSubmit(plain, k, 140, dummy);
             }
         }
-        printf("%lx\n", plain);
-        for (int k = 0; k < 8; k++) {
-            for (int c = 0; c < 999999; c++)
-                A5CudaSubmit(plain, k, 140, dummy);
+    }
+}
+
+void push_burst_2000()
+{
+    int *dummy;
+    uint64_t feed = 0xfeacdfea13456760;
+    uint64_t plain = 0;
+    for (int i=0; i < 125; i++) {
+        for (int j=0; j < 8; j++) {
+           plain = feed + i*j;
+           A5CudaSubmit(plain, j, 140, (void**)dummy);
         }
     }
-    while (not A5CudaPopResult(start_val, stop_val, (void**) &dummy)) {
+    for (int i=0; i < 125; i++) {
+        for (int j=0; j < 8; j++) {
+           plain = feed + i*j;
+           A5CudaSubmitPartial(plain, j, 140, (void**)dummy);
+        }
     }
-    struct timeval stop_time;
-    gettimeofday(&stop_time, NULL);
-    unsigned long diff = 1000000 * (stop_time.tv_sec - start_time.tv_sec);
-    diff += stop_time.tv_usec - start_time.tv_usec;
-    printf("%i msec\t\n", (int) (diff / 1000));
+}
+
+void pop_result(int n)
+{
+    uint64_t start_val;
+    uint64_t stop_val;
+    int* dummy;
+    for (int i=0; i < n; i++) {
+        while (not A5CudaPopResult(start_val, stop_val, (void**)dummy)) {
+            usleep(100);
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    A5CudaInit(8,12);
+    push_burst_2000();
+    pop_result(2000);
     A5CudaShutdown();
 }
+
